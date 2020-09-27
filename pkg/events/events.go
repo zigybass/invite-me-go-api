d738bda06905f6b9acc2803364a60691ef3ebe4f
Zigy <zigybass@gmail.com>
2020-09-27 16:07:31 -0400

    getEvents now returns correct array of events that are not softDeleted.

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -28,13 +28,14 @@ var Db = newEventHandlers()
 
 func (h *eventHandlers) GetEvents(w http.ResponseWriter, r *http.Request) {
 	cors.SetupCORS(&w, r)
-	events := make([]Event, len(h.store))
+
+	var events []Event
 
 	h.Lock()
 	i := 0
 	for _, event := range h.store {
 		if event.SoftDeleted != true {
-			events[i] = event
+			events = append(events, event)
 		}
 		i++
 	}
@@ -56,6 +57,7 @@ func (h *eventHandlers) AddEvent(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodOptions {
 		return
 	}
+
 	bodyBytes, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
@@ -137,7 +139,7 @@ func (h *eventHandlers) DeleteEvent(w http.ResponseWriter, r *http.Request) {
 	h.Lock()
 	event, ok := h.store[i]
 	event.SoftDeleted = true
-	h.store[i] = event
+	h.store[event.Id] = event
 	h.Unlock()
 
 	if !ok {
